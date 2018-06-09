b38fb2dc1697e0265d890733d87ac4328eeccf1d
Leandro Souza <Leandro.Souza@dominos.co.uk>
2018-06-09 16:02:31 +0100

    SakTextInputLayout

--- src/sak-textinputlayout/sak-textinputlayout.common.d.ts
+++ src/sak-textinputlayout/sak-textinputlayout.common.d.ts
@@ -1,4 +1,6 @@
 import { SakTextInputLayout as SakTextInputLayoutDefinition } from "./sak-textinputlayout";
 import { View } from "tns-core-modules/ui/core/view";
+
 export declare abstract class SakTextInputLayoutBase extends View implements SakTextInputLayoutDefinition {
+    
 }
